10688e1329d0b350f3cbe540fc7510902aabfd81
Jaks-Hat <flofdu25@hotmail.fr>
2017-11-14 09:31:56 +0100

    Corrected loop

--- mesures_script.sh
+++ mesures_script.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-taille_vecteur = 1000
-taille_vecteur_max = 1000000
-nbthreads = 1
-while [$taille_vecteur -le $taille_vecteur_max]
+taille_vecteur=1000
+taille_vecteur_max=1000000
+nbthreads=1
+while [ $taille_vecteur -le $taille_vecteur_max ]
 do
-  ./creer_vecteur -s taille_vecteur >vecteur.txt
-  ./tri_sequentiel --time --quiet --rusage <vecteur.txt
-  ./tri_threads --time --rusage --quiet --parallelism 4 <vecteur.txt
-  $taille_vecteur = $taille_vecteur+1000
+  ./creer_vecteur -s taille_vecteur > vecteur.txt
+  #./tri_sequentiel --time --quiet --rusage < vecteur.txt
+  #./tri_threads --time --rusage --quiet --parallelism 4 < vecteur.txt
+  taille_vecteur=$(($taille_vecteur + 1000))
 done
+
+#./creer_vecteur -s taille_vecteur >vecteur.txt
